a6133ca6f7581a29476a8baf807d2c0a24c0a37a
Katsushi OUGI <bdbkx286@gmail.com>
2019-03-22 00:16:37 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,92 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
-const ENV = process.env.NODE_ENV || 'development';
-const isProd = ENV !== 'development';
-const output = isProd ? 'build' : 'public';
+const ENV = process.env.NODE_ENV || "development"
+const isProd = ENV !== "development"
+const output = isProd ? "build" : "public"
 
 module.exports = {
-  entry: ['./src/index.tsx'],
+  entry: ["./src/index.tsx"],
   output: {
-    filename: 'js/[name].[hash].js',
-    chunkFilename: 'js/[id].[hash].js',
-    publicPath: '/',
+    filename: "js/[name].[hash].js",
+    chunkFilename: "js/[id].[hash].js",
+    publicPath: "/",
     path: path.resolve(__dirname, output)
   },
   optimization: {
     splitChunks: {
-      name: 'vendor',
-      chunks: 'initial'
+      name: "vendor",
+      chunks: "initial"
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html',
+      template: "public/index.html",
+      filename: "index.html",
       minify: {
         collapseWhitespace: true
       }
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[hash].css',
-      chunkFilename: 'css/[id].[hash].css'
+      filename: "css/[name].[hash].css",
+      chunkFilename: "css/[id].[hash].css"
     }),
     // 型エラーのみを検知する
     new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })
-  ].concat(isProd ? [new CleanWebpackPlugin()] : []),
+  ].concat(
+    isProd
+      ? [
+          new CleanWebpackPlugin(),
+          new CopyWebpackPlugin([
+            { from: "**/*", to: ".", context: "public", ignore: ["index.html"] }
+          ])
+        ]
+      : []
+  ),
   module: {
     rules: [
       {
         test: /\.*(ts|tsx)$/,
         exclude: /node_modules/,
-        use: [
-          {
-            // webpackをマルチスレッド化
-            loader: 'thread-loader',
-            options: {
-              workers: require('os').cpus().length - 1,
-              poolTimeout: Infinity
-            }
-          },
-          {
-            loader: 'babel-loader'
-          },
-          {
-            loader: 'ts-loader',
-            options: {
-              // 型を無視する
-              happyPackMode: true
-            }
-          }
-        ]
+        use: [].concat(
+          isProd
+            ? [
+                {
+                  loader: "babel-loader"
+                },
+                {
+                  loader: "ts-loader",
+                  options: {
+                    // 型を無視する
+                    happyPackMode: true
+                  }
+                }
+              ]
+            : [
+                {
+                  // webpackをマルチスレッド化
+                  loader: "thread-loader",
+                  options: {
+                    workers: require("os").cpus().length - 1,
+                    poolTimeout: Infinity
+                  }
+                },
+                {
+                  loader: "babel-loader"
+                },
+                {
+                  loader: "ts-loader",
+                  options: {
+                    // 型を無視する
+                    happyPackMode: true
+                  }
+                }
+              ]
+        )
       },
       {
         test: /\.css$/,
@@ -69,11 +94,11 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader
           },
-          'css-loader',
+          "css-loader",
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
-              plugins: [].concat(isProd ? [require('cssnano')] : [])
+              plugins: [].concat(isProd ? [require("cssnano")] : [])
             }
           }
         ]
@@ -82,10 +107,10 @@ module.exports = {
         test: /\.(png|jpg|gif)$/i,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              name: 'img/[hash].[ext]',
-              fallback: 'file-loader'
+              name: "img/[hash].[ext]",
+              fallback: "file-loader"
             }
           }
         ]
@@ -93,10 +118,10 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.css']
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css"]
   },
   devServer: {
-    contentBase: 'public',
-    host: 'localhost'
+    contentBase: "public",
+    host: "localhost"
   }
-};
+}
